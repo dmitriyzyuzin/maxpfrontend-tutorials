93a49347451d41d07cb7a1dacfd7eb553da8f8f0
dmitriyzyuzin <d.zyuzin@beauit.com>
2019-03-19 01:58:27 +0300

    connected router

--- tz-1/src/App.js
+++ tz-1/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import configureStore from './store/configureStore';
 import initialState from './store/initialState';
+import News from './components/News';
+import Profile from "./components/Profile";
 
 const store = configureStore(initialState);
 
@@ -9,7 +13,27 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        empty
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Главная</Link>
+              </li>
+              <li>
+                <Link to='/login'>Войти</Link>
+              </li>
+              <li>
+                <Link to='/news'>Новости</Link>
+              </li>
+              <li>
+                <Link to='/profile'>Профиль</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path='/news' component={News} />
+          <Route path='/profile' component={Profile} />
+        </Router>
       </Provider>
     );
   }
